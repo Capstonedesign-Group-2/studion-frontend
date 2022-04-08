3c351b6732768dbd8a1eeaaf465f67cc765467ce
lizill <lizill@naver.com>
2022-04-08 15:44:53 +0900

    [ADD] Turn server

--- pages/room/[id].tsx
+++ pages/room/[id].tsx
@@ -22,6 +22,11 @@ const pc_config = {
 		{
 			urls: process.env.NEXT_PUBLIC_ICE_STUN_SERVER || 'stun:stun.l.google.com:19302',
 		},
+		{
+			urls: process.env.NEXT_PUBLIC_ICE_TURN_SERVER,
+			credential: process.env.NEXT_PUBLIC_ICE_TURN_SERVER_PASSWORD,
+			username: process.env.NEXT_PUBLIC_ICE_TURN_SERVER_USERNAME
+		}
 	],
 }
 
