fbd3b4586f377e92e32c1074b48904c0d0ce7c11
kki4504 <rhkddlf9876@gmail.com>
2022-10-02 16:21:26 +0900

    EDIT: chat page localization

--- components/soundCloud/chat/ChatDropdown.jsx
+++ components/soundCloud/chat/ChatDropdown.jsx
@@ -1,7 +1,8 @@
 import Router from "next/router"
 import Socket from "../../../socket/index"
+import useTranslation from "next-translate/useTranslation"
 const ChatDropdown = ({userData, otherUser}) => {    
-    
+    const { t } = useTranslation("chat")
     const onClickExit = () => {
         Socket.socket.emit('chat_exit', {
             room_id: Router.query.id,
@@ -15,7 +16,7 @@ const ChatDropdown = ({userData, otherUser}) => {
     return(
         <div className="">
             <p className="cursor-pointer font-semibold  text-red-700 hover:text-red-500 text-center" onClick={onClickExit}>
-                出る
+                {t("dropBox_exit_btn")}
             </p>
         </div>
     )
